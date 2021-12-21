a245ca087e678975477c62d2f0c4e58b241c3a79
user <user@408-07.kpc408.ru>
2021-12-21 08:59:29 +0300

    тестирование

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static List<int> Method(List<int> numbers)
+        {// 0
+            List<int> evenNumbers = new List<int>(); //  1
+
+            for (int i = 0; i < numbers.Count; i++) // 2
+            {
+                if (numbers[i] % 2 == 0) // 3
+                {
+                    evenNumbers.Add(numbers[i]); // 4
+                }
+            } // 5
+
+            return evenNumbers; // 6
+        } // 7
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        List<int> list1 = new List<int>();
+        List<int> list2 = new List<int>() { 1, 2, 3, 4 };
+        List<int> list3 = new List<int>() { 2, 4 };
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.ReferenceEquals(list1, Class1.Method(new List<int>()));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.ReferenceEquals(list2, Class1.Method(list3));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.ReferenceEquals(list2, Class1.Method(list2));
+        }
+    }
+}
